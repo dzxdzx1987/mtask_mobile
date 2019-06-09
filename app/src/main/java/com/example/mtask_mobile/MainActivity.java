6b9be707a7cf642be3091b4f55bb91e94248fa16
Kevin <dzxdzx1987@gmail.com>
2019-06-09 14:18:00 +0900

    modified toolbar

--- app/src/main/java/com/example/mtask_mobile/MainActivity.java
+++ app/src/main/java/com/example/mtask_mobile/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mtask_mobile;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background);
+        }
+
     }
 }
